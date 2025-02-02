6c75e72019458af0e340760a495dcdfc9426d021
champ-sk <shubhamkumar3183@gmail.com>
2025-02-02 12:52:39 +0530

    additional changes

--- src/Login.js
+++ src/Login.js
@@ -1,46 +1,61 @@
 import { useState } from "react";
 
-const Login = ()=>{
-    const [userName  , setUserName]  =useState("");
-    const [password, setPassword] = useState("");
-    const [isLoggedIn , setIsLoggedIn] = useState(false) ;
+const Login = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-    const handleSubmit = (e)=>{
-e.preventDefault();
-if(!userName.trim() && !password.trim()){
-    setIsLoggedIn(true) ;
-}
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (userName ==="user" && password ==="password") {
+      setMessage("Welcome user!");
+      setIsSubmitted(true);
+    }else{
+        setMessage("Invalid username or password");
+        
     }
+    setUserName("");
+    setPassword("");
+  };
 
-    return(
-        <div>
-            <h1>Login Page </h1>
-{isLoggedIn? (
-    <h2 className="text-xl font-bold">Welcome, {userName}!</h2>
-):(
-    <>
-     <form onSubmit={handleSubmit}>
-                <h4>UserName</h4>
-                <input
-                type="text"
-                placeholder="username"
-                onChange={(e)=>setUserName(e.target.value)}/>
-                <h4>Password</h4>
-                <input
-                type="text"
-                placeholder="password"
-                onChange={(e)=>setPassword(e.target.value)}
-                />
-                <button type="submit">
-                    Submit
-                </button>
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1> Login Page</h1>
+        {isSubmitted ? (<p>{message}</p>):(
 
-            </form>
-    </>
-)}
-           
-        </div>
-    )
-}
+<form onSubmit={handleSubmit}>
+<div>
+  <label>Username</label>
+  <br />
+  <input
+    type="text"
+    value={userName}
+    onChange={(e) => setUserName(e.target.value)}
+    placeholder="Enter username"
+    required
+  />
+</div>
+<br />
+<div>
+  <label>Password</label>
+  <br />
+  <input
+    type="password"
+    value={password}
+    onChange={(e) => setPassword(e.target.value)}
+    placeholder="Enter password"
+    required
+  />
+</div>
+<br />
+<button type="submit">Submit</button>
+</form>
+        )}
+        
+       
+       
+      </div>
+  );
+};
 export default Login;
